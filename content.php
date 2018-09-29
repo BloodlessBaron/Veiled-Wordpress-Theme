3044b047cd1d3605300f1b5ee985b7f252c95de4
Juan Montellano <bloodlessbaron@protonmail.com>
2018-09-28 22:48:03 -0500

    removed author link

--- content.php
+++ content.php
@@ -1,9 +1,9 @@
 <div class="post-wrapper">
   <div class="post-title">
-    <h2><?php the_title(); ?></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </div>
   <div class="post-meta">
-    By <a href="#"><?php the_author(); ?> on <?php the_date(); ?></a>
+    By <?php the_author(); ?> on <?php the_date(); ?>
   </div>
   <div class="post-content">
     <?php the_content(); ?>
