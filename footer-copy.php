5f8fb1632652ea1e700d18f0b50af8348ab4edab
John Barner <bloodlessbaron@protonmail.com>
2018-10-22 16:09:18 -0500

    added theme url

--- footer-copy.php
+++ footer-copy.php
@@ -1,2 +1,2 @@
 
-&copy; <?php echo date("Y"). ' ' . get_bloginfo('name'); ?>.
+&copy; <?php echo date("Y"). ' ' . get_bloginfo('name'); ?>. <a href="https://github.com/BloodlessBaron/Veiled-Wordpress-Theme">Veiled Wordpress Theme</a>.
