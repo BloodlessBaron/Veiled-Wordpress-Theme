f22ee8be728244668bbade7a13b2c388281a9103
John Barner <bloodlessbaron@protonmail.com>
2018-10-23 13:22:20 -0500

    added category and tags to post meta, fixes #6

--- content.php
+++ content.php
@@ -3,7 +3,7 @@
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </div>
   <div class="post-meta">
-    By <?php the_author(); ?> on <?php the_date(); ?>.<br />
+    By <?php the_author(); ?> in <?php the_category(', '); ?> on <?php the_date(); ?>.<br /><?php the_tags(); ?><br />
     This post has <a href="<?php the_permalink(); ?>#comments"><?php comments_number( 'no responses', 'one response', '% responses' ); ?></a>.
   </div>
   <div class="post-content">
