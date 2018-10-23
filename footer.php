11d0fe0d7d7de5e5d4b42ceae8f518a1ac0890c2
John Barner <bloodlessbaron@protonmail.com>
2018-10-22 21:37:24 -0500

    got rid of duplicate code, fixes #22

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
 </div>
-<?php wp_footer(); ?> 
+<div id="footer">
+  <p class="footer-copy"><?php get_footer('copy'); ?></p>
+</div>
+</div>
+<div id="rightbar">
+  <?php get_sidebar('right-bar'); ?>
+</div>
+</div>
+  <?php wp_footer(); ?>
 </body>
 </html>
