284f2f9173132f581e2d6471d91341fa03060559
John Barner <bloodlessbaron@protonmail.com>
2018-10-22 21:59:37 -0500

    added footer widget code

--- footer-copy.php
+++ footer-copy.php
@@ -1,2 +1,4 @@
-
 &copy; <?php echo date("Y"). ' ' . get_bloginfo('name'); ?>. Powered by the <a href="https://github.com/BloodlessBaron/Veiled-Wordpress-Theme">Veiled Theme</a>.
+<div id="footer-widgets">
+  <?php dynamic_sidebar( 'footer' ); ?>
+</div>
