02d108e447e121e913bdc86ee1257e9782985dae
John Barner <bloodlessbaron@protonmail.com>
2018-10-23 21:12:24 -0500

    added and styled pagination

---
+++ content-pagination.php
@@ -0,0 +1,5 @@
+<hr />
+<div class="pagination"><h1>Pagination:</h1></div>
+<div class="nav-previous alignleft"><h3><?php previous_posts_link( '<<< Newer posts' ); ?></h3></div>
+<div class="nav-next alignright"><h3><?php next_posts_link( 'Older posts >>>' ); ?></h3></div>
+<div class="clear">&nbsp;</div>
