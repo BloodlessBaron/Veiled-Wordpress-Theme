dbda93db2e0451c645012e2826b7582a664be649
John Barner <bloodlessbaron@protonmail.com>
2018-10-24 10:04:45 -0500

    fiddled with how pagination looks some more

--- content-pagination.php
+++ content-pagination.php
@@ -1,5 +1,3 @@
-<hr />
-<div class="pagination"><h1>Pagination:</h1></div>
-<div class="nav-previous alignleft"><h3><?php previous_posts_link( '<<< Newer posts' ); ?></h3></div>
-<div class="nav-next alignright"><h3><?php next_posts_link( 'Older posts >>>' ); ?></h3></div>
+<div class="nav-previous alignleft"><h2><?php previous_posts_link( '<<< Newer posts' ); ?></h2></div>
+<div class="nav-next alignright"><h2><?php next_posts_link( 'Older posts >>>' ); ?></h2></div>
 <div class="clear">&nbsp;</div>
